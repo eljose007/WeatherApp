4d5af8bcfdd5dc5de1f39c5569081639d21e31a6
eljose007 <eljose007@users.noreply.github.com>
2018-10-06 12:33:05 -0300

    cambios redux

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import LocationList from './components/LocationList'
+import { createStore } from 'redux';
 import './App.css';
 import ForecastExtended from './components/ForecastExtended';
 
@@ -18,6 +19,8 @@ const cities = [
   'Lima,pe',
 ]
 
+
+const store = createStore(() => {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 class App extends Component {
 
   constructor(){
@@ -27,6 +30,9 @@ class App extends Component {
   }
   handleSelectedLocation = city => {
     this.setState({ city });
+
+    const action = { type: 'setCity', value: city };
+    store.dispatch(action);
   };
   render() {
     const { city } = this.state;
