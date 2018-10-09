b6dca47e6e048a6a27f5173833fc97addd4224c7
eljose007 <eljose007@users.noreply.github.com>
2018-10-08 23:58:41 -0300

    122

---
+++ src/containers/LocationListContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTyes from 'prop-types';
+
+class LocationListContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+LocationListContainer.propTypes = {
+    setCity: PropTypes.func.isRequired,
+}
+
+export default LocationListContainer;
